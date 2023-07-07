4472906dd67cafc2361cfb4742aad70cbf288e68
KhanhLinh1304 <110154279+KhanhLinh1304@users.noreply.github.com>
2023-07-07 16:47:41 +0700

    Add files via upload

--- src/main/java/dao/IPostDAO.java
+++ src/main/java/dao/IPostDAO.java
@@ -7,7 +7,10 @@ import java.util.List;
 public interface IPostDAO {
 
     public List<PostModel> getPost () ;
+    public List<PostModel> getPostByIdUser(int id_user);
     public void addPost(PostModel post);
     public void updatePost(PostModel post);
     public void deletePost(PostModel post);
+    public PostModel getPostByIdPost(int idPost);
+ 
 }
